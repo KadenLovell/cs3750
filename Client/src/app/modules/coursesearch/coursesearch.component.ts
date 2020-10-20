36fa17601fb0858af2e0bf2a2b2e875077717d93
Dusti <dustinchenson1996@gmail.com>
2020-10-19 22:39:25 -0600

    added coursesearch module

---
+++ Client/src/app/modules/coursesearch/coursesearch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coursesearch',
+  templateUrl: './coursesearch.component.html',
+  styleUrls: ['./coursesearch.component.scss']
+})
+export class CourseSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
