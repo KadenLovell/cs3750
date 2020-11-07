5515ddecd0d1dfd7d2a509d5a270e69ddd9fd8c9
Kaden Lovell <kadenlovell@live.com>
2020-11-07 03:53:06 -0700

    checkpiont for courses, and assignments

--- Client/src/app/modules/course/course.component.ts
+++ Client/src/app/modules/course/course.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from '../../base/base.component';
+
+// shared
+import { User } from "../../shared/user/user";
+import { UserService } from "../../shared/user/user.service";
 
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.scss']
 })
-export class CourseComponent implements OnInit {
+export class CourseComponent extends BaseComponent implements OnInit {
+  model: any;
+  rows: any;
+  errors: any;
+  view: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get user(): User {
+    return this._userService.user;
   }
 
+  constructor(private readonly _userService: UserService) {
+    super();
+  }
+
+  ngOnInit(): void {
+    this.view = 1;
+    this.model = {};
+  }
 }
