33662790824b25cd7c12ff8bf991b376ce3b5c51
Austin Duran <austin.duran39@gmail.com>
2020-11-17 19:05:49 -0700

    committing unit tests for assignments

--- ServerTesting/UnitTest1.cs
+++ ServerTesting/UnitTest1.cs
@@ -1,16 +1,63 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.EntityFrameworkCore;
+using Server.Models;
+using Server.Controllers;
+using System;
+using Server.Persistence;
+using System.Threading.Tasks;
+using System.Data.Entity.Infrastructure;
 
 namespace ServerTesting
 {
     [TestClass]
     public class UnitTest1
     {
+        public DataContext _context { get; private set; }
+
         [TestMethod]
-        public void TestMethod1()
+        public async Task AssignmentTestAsync()
         {
-            Math math = new Math();
-            int answer = math.add(1, 2);
-            Assert.AreEqual(answer, 3);
+            DbContextOptions<DataContext> options = new DbContextOptions<DataContext>();
+            DbContextOptionsBuilder builder = new DbContextOptionsBuilder(options);
+            SqlServerDbContextOptionsExtensions.UseSqlServer(builder, "Data Source=localhost;Initial Catalog=LMS-sandbox;Integrated Security=True;MultipleActiveResultSets=True;", null);
+            _context = new DataContext((DbContextOptions<DataContext>)builder.Options);
+            var assignments = await _context.Assignment.ToListAsync();
+            var courses = await _context.Course.ToListAsync();
+            long courseId = 0;
+            if (courses.Count > 0)
+            {
+                for (int i = 0; i < courses.Count; i++)
+                {
+                    var currentNumAssignments = 0;
+                    var newNumAssignments = 0;
+                    courseId = courses[i].Id;
+                    for (int j = 0; j < assignments.Count; j++) {
+                        if (assignments[j].CourseId == courseId) {
+                            currentNumAssignments++;
+                        }
+                    }
+                    Assignment assignment = new Assignment();
+                    assignment.Title = "Test Assignment";
+                    assignment.DueDate = DateTime.Now;
+                    assignment.DueTime = null;
+                    assignment.CourseId = courseId;
+                    assignment.AssignmentType = 0;
+                    assignment.MaxPoints = 100;
+                    assignment.CreatedDate = DateTime.Now;
+                    _context.Assignment.Add(assignment);
+                    _context.SaveChanges();
+                    assignments = await _context.Assignment.ToListAsync();
+                    courses = await _context.Course.ToListAsync();
+                    for (int k = 0; k < assignments.Count; k++)
+                    {
+                        if (assignments[k].CourseId == courseId)
+                        {
+                            newNumAssignments++;
+                        }
+                    }
+                    Assert.AreEqual(newNumAssignments, currentNumAssignments + 1);
+                }
+            }
         }
     }
 }
