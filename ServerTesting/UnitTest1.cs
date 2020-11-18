3eae16ff40aea4d11d28652e235b2e5848aa4be9
Dusti <dustinchenson1996@gmail.com>
2020-11-17 20:01:08 -0700

    added course add unit test to UnitTest1.cs

--- ServerTesting/UnitTest1.cs
+++ ServerTesting/UnitTest1.cs
@@ -59,5 +59,63 @@ namespace ServerTesting
                 }
             }
         }
+
+        [TestMethod]
+        public async Task CourseAddTestAsync()
+        {
+            DbContextOptions<DataContext> options = new DbContextOptions<DataContext>();
+            DbContextOptionsBuilder builder = new DbContextOptionsBuilder(options);
+            SqlServerDbContextOptionsExtensions.UseSqlServer(builder, "Data Source=localhost;Initial Catalog=LMS-sandbox;Integrated Security=True;MultipleActiveResultSets=True;", null);
+            _context = new DataContext((DbContextOptions<DataContext>)builder.Options);
+
+            var courses = await _context.Course.ToListAsync();
+            var users = await _context.User.ToListAsync();
+
+            long temp = 0;
+            for (int i = 0; i < users.Count; i++) { // loop through the users and find an admin
+                if (users[i].Role == 1) {
+                    temp = users[i].Id;
+                }
+            }
+            if (temp == 0) {
+                // create a new user of type instructor
+            }
+
+            int oldInstructorCourseCount = 0;
+            int newInstructorCourseCount = 0;
+
+            for (int i = 0; i<courses.Count; i++) {
+                if (courses.InstructorId == temp) { oldInstructorCourseCount++; }
+            }
+
+            Course course = new Course();
+            course.InstructorId = temp;     //assigns the new course to an instructor
+            course.Code = "test";
+            course.Name = "test";
+            course.Instructor = "test, test";
+            course.Description = "test";
+            course.Department = "Computer Science";
+            course.CreditHours = 20;
+            course.Location = "Main Campus";
+            course.StartDate = new DateTime();
+            course.StartTime = new DateTime();
+            course.EndTime = new DateTime();
+            course.Capacity = 20;
+            course.Monday = 1;
+            course.Tuesday = 1;
+            course.Wednesday = 1;
+            course.Thursday = 1;
+            course.Friday = 1;
+            _context.Course.Add(course);
+            _context.SaveChanges();
+
+            var courses = await _context.Course.ToListAsync();
+            for (int i = 0; i<courses.Count; i++) {
+                if (courses.InstructorId == temp) { newInstructorCourseCount++; }
+            }
+
+            Assert.AreEqual(oldInstructorCourseCount+1, newInstructorCourseCount);
+        }
+
     }
 }
